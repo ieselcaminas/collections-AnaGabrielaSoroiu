91f05bc765911a351ead84e2365a7e0e4f85c417
anasor2 <anasor2@alu.edu.gva.es>
2024-11-26 12:59:39 +0100

    Coches

---
+++ src/Coches.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class Coches {
+    public static void main(String[] args) {
+        ArrayList<String> coches = new ArrayList<>();
+        String marca;
+
+        Scanner sc = new Scanner(System.in);
+
+        do {
+            marca = sc.nextLine();
+            if (!marca.isEmpty()) {
+                coches.add(marca);
+            }
+        } while (!marca.isEmpty());
+
+        Collections.sort(coches);
+
+        for (int i = 0; i < coches.size() ; i++) {
+            System.out.println(coches.get(i));
+        }
+
+    }
+}
