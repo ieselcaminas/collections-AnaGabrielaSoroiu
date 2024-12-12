08bfb65d9bb99c8f58c4a801619806ce9edc4d67
anasor2 <anasor2@alu.edu.gva.es>
2024-12-12 13:49:50 +0100

    No acabado

---
+++ src/Refuerzo/Mediana.java
@@ -0,0 +1,18 @@
+package Refuerzo;
+
+public class Mediana {
+    public static void main(String[] args) {
+        int[] numeros = {1, 2, 6, 17, 18, 22, 35, 46, 109, 143, 200};
+
+    }
+
+    public static double mediana(int[] numeros) {
+        double mediana = 0;
+
+        for (int i = 0; i < numeros.length; i++) {
+            if (numeros[i] % 2 == 0) {
+
+            }
+        }
+    }
+}
