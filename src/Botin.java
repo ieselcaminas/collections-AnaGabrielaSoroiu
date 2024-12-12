a6cfdb5f917626e8345ba4bb1e5102b2a8ec197f
anasor2 <anasor2@alu.edu.gva.es>
2024-12-12 12:28:20 +0100

    Botin

--- src/Botin.java
+++ src/Botin.java
@@ -4,33 +4,24 @@ import java.util.Map;
 public class Botin {
 
     public static void main(String[] args) {
-        Map<Integer, String> repartir = botin(3, new String[] {"10, 20, 50, 200, 500"});
+        Map<Integer, String> repartir = botin(3, new String[] {"10", "20", "50", "200", "500"});
 
         System.out.println(repartir.toString());
-
     }
 
     public static Map<Integer, String> botin (int ladrones, String[] billetes) {
         Map<Integer, String> botin = new HashMap<>();
         int leToca;
-        String billete;
-        String reparto;
 
         for (int i = 0; i < billetes.length; i++) {
-            billete = billetes[i];
-
             //Si devuelve 0, tiene que volver a empezar por el primero.
             leToca = i % ladrones;
 
-            reparto = botin.get(leToca);
-
-            if (botin == null) {
-                botin.put(leToca, reparto);
+            if (!botin.containsKey(leToca)) {
+                botin.put(leToca, billetes[i]);
             } else {
-                botin.put(leToca, reparto + " " + billete);
+                botin.put(leToca, billetes[i] + " " + botin.get(leToca));
             }
         } return botin;
-
-
     }
 }
